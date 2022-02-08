90990d4d82e66e91989da1f372fdf638713075dd
webtracer <74070641+webtracer@users.noreply.github.com>
2022-02-08 08:49:01 -0800

    Add files via upload

---
+++ String_Challenge/String_Challenge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace String_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+
+            Console.Write("Please enter your name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine($"Hello there {name}");
+            Console.WriteLine($"Hello there {name.ToLower()}");
+            Console.WriteLine($"Hello there {name.ToUpper()}");
+            Console.WriteLine($"Hello there {name.Trim()}");
+            Console.WriteLine($"Hello there {name.Substring(0,5)}");
+            Console.Read();
+        }
+    }
+}
---
+++ String_Challenge2/String_Challenge2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace String_Challenge2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userString;
+            char searchChar;
+            
+            Console.Write("Please enter any string: ");
+            userString = Console.ReadLine();
+            Console.WriteLine("Please enter a character to find in your string: ");
+            searchChar = Console.ReadLine()[0];
+
+            int searchingChar = userString.IndexOf(searchChar);
+            Console.WriteLine($"The index of character {searchChar} in your string is {searchingChar}");
+
+            Console.Write("Enter your first name: ");
+            string fname = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            string lname = Console.ReadLine();
+            string fullName = string.Concat(fname," ", lname);
+            Console.WriteLine($"Your full name is {fullName}");
+        }
+    }
+}
